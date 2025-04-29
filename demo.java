96959c60c5e11dec610a6bad6800f0b50f2925e3
aravindreddy <aravindreddyashireddy@gmail.com>
2025-04-29 11:17:05 +0530

    Second Commit

---
+++ demo.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+import java.net.URL;
+
+public class testdemo {
+
+    @Test
+    public void runTestOnChrome() throws Exception {
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL(" http://192.168.136.45:4444/wd/hub"), capabilities);
+        
+        driver.get("https://www.google.com");
+        System.out.println("Page title on Chrome: " + driver.getTitle());
+        driver.quit();
+    }
+
+    @Test
+    public void runTestOnFirefox() throws Exception {
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        WebDriver driver = new RemoteWebDriver(new URL(" http://192.168.136.45:4444/wd/hub"), capabilities);
+        
+        driver.get("https://www.google.com");
+        System.out.println("Page title on Firefox: " + driver.getTitle());
+        driver.quit();
+    }
+}
